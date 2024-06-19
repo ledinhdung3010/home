2872253d8d8757b02a4aec37e216f5764702c5c2
ledinhdung3010 <ledinhdung3010@gmail.com>
2024-06-19 11:07:08 +0700

    update

--- js/main.js
+++ js/main.js
@@ -25,3 +25,80 @@ $('.btn-next').on('click', function() {
         nextBtn.addClass('selected');
     }
 });
+$(document).ready(function() {
+    function toggleDetails() {
+        if ($(this).text() === "Xem thêm") {
+            $(this).text("Ẩn đi");
+            $(".item-directory").show();
+        } else {
+            $(this).text("Xem thêm");
+            $(".item-directory").slice(6).hide();
+        }
+    }
+
+    function handleResize() {
+            $(".item-directory").slice(6).hide();
+            $("#toggle-details").on("click", toggleDetails);
+        
+    }
+
+    handleResize(); // Gọi hàm một lần khi tải trang để thiết lập ban đầu
+});
+$(document).ready(function() {
+    function toggleDetails() {
+        if ($(this).text() === "Xem thêm") {
+            $(this).text("Ẩn đi");
+            $(".item-product__sale").show();
+        } else {
+            $(this).text("Xem thêm");
+            $(".item-product__sale").slice(4).hide();
+        }
+    }
+
+    function handleResize() {
+            $(".item-product__sale").slice(4).hide();
+            $("#toggle-sale").on("click", toggleDetails);
+        
+    }
+
+    handleResize(); // Gọi hàm một lần khi tải trang để thiết lập ban đầu
+});
+$(document).ready(function() {
+    function toggleDetails() {
+        if ($(this).text() === "Xem thêm") {
+            $(this).text("Ẩn đi");
+            $(".item-product__favorite").show();
+        } else {
+            $(this).text("Xem thêm");
+            $(".item-product__favorite").slice(4).hide();
+        }
+    }
+
+    function handleResize() {
+            $(".item-product__favorite").slice(4).hide();
+            $("#toggle-favorite").on("click", toggleDetails);
+        
+    }
+
+    handleResize(); // Gọi hàm một lần khi tải trang để thiết lập ban đầu
+});
+$(document).ready(function() {
+    function toggleDetails() {
+        if ($(this).text() === "Xem thêm") {
+            $(this).text("Ẩn đi");
+            $(".item-product__trend").show();
+        } else {
+            $(this).text("Xem thêm");
+            $(".item-product__trend").slice(4).hide();
+        }
+    }
+
+    function handleResize() {
+            $(".item-product__trend").slice(4).hide();
+            $("#toggle-trend__buy").on("click", toggleDetails);
+        
+    }
+
+    handleResize(); // Gọi hàm một lần khi tải trang để thiết lập ban đầu
+});
+
